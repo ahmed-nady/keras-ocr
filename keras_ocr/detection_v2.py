96a2032a0c4935b286a8c715043cef5eef35ab6d
Ahmed Nady <ahmednady@fci.helwan.edu.eg>
2020-07-12 10:59:55 +0200

    add detect_multi_scale

--- keras_ocr/detection_v2.py
+++ keras_ocr/detection_v2.py
@@ -268,7 +268,87 @@ class UpsampleLike(keras.layers.Layer):
         else:
             return (input_shape[0][0], ) + input_shape[1][1:3] + (input_shape[0][-1], )
 
+def getBoxes_multi_scale(y_pred,
+             detection_threshold=0.7,
+             text_threshold=0.4,
+             link_threshold=0.4,
+             size_threshold=10):
+    box_groups = []
+    # Prepare data
+    textmap_scale_1 = y_pred[0][..., 0].copy()
+    linkmap_scale_1 = y_pred[0][..., 1].copy()
+    textmap_scale_2 = y_pred[1][..., 0].copy()
+    linkmap_scale_2 = y_pred[1][..., 1].copy()
 
+    #resize text_map_scale-1 to ttx_map_scale_2
+    textmap = cv2.resize(textmap_scale_1,textmap_scale_2.shape) + textmap_scale_2
+    linkmap = cv2.resize(linkmap_scale_1,linkmap_scale_2.shape) + linkmap_scale_2
+    score_map = cvt2HeatmapImg(textmap)
+    link_map = cvt2HeatmapImg(linkmap)
+
+    img_h, img_w = textmap.shape
+
+    _, text_score = cv2.threshold(textmap,
+                                  thresh=text_threshold,
+                                  maxval=1,
+                                  type=cv2.THRESH_BINARY)
+    _, link_score = cv2.threshold(linkmap,
+                                  thresh=link_threshold,
+                                  maxval=1,
+                                  type=cv2.THRESH_BINARY)
+    n_components, labels, stats, _ = cv2.connectedComponentsWithStats(np.clip(
+        text_score + link_score, 0, 1).astype('uint8'),
+                                                                      connectivity=4)
+    boxes = []
+    for component_id in range(1, n_components):
+        # Filter by size
+        size = stats[component_id, cv2.CC_STAT_AREA]
+
+        if size < size_threshold:
+            continue
+
+        # If the maximum value within this connected component is less than
+        # text threshold, we skip it.
+        if np.max(textmap[labels == component_id]) < detection_threshold:
+            continue
+
+        # Make segmentation map. It is 255 where we find text, 0 otherwise.
+        segmap = np.zeros_like(textmap)
+        segmap[labels == component_id] = 255
+        segmap[np.logical_and(link_score, text_score)] = 0
+        x, y, w, h = [
+            stats[component_id, key] for key in
+            [cv2.CC_STAT_LEFT, cv2.CC_STAT_TOP, cv2.CC_STAT_WIDTH, cv2.CC_STAT_HEIGHT]
+        ]
+
+        # Expand the elements of the segmentation map
+        niter = int(np.sqrt(size * min(w, h) / (w * h)) * 2)
+        sx, sy = max(x - niter, 0), max(y - niter, 0)
+        ex, ey = min(x + w + niter + 1, img_w), min(y + h + niter + 1, img_h)
+        segmap[sy:ey, sx:ex] = cv2.dilate(
+            segmap[sy:ey, sx:ex],
+            cv2.getStructuringElement(cv2.MORPH_RECT, (1 + niter, 1 + niter)))
+
+        # Make rotated box from contour
+        contours = cv2.findContours(segmap.astype('uint8'),
+                                    mode=cv2.RETR_TREE,
+                                    method=cv2.CHAIN_APPROX_SIMPLE)[-2]
+        contour = contours[0]
+        box = cv2.boxPoints(cv2.minAreaRect(contour))
+
+        # Check to see if we have a diamond
+        w, h = np.linalg.norm(box[0] - box[1]), np.linalg.norm(box[1] - box[2])
+        box_ratio = max(w, h) / (min(w, h) + 1e-5)
+        if abs(1 - box_ratio) <= 0.1:
+            l, r = contour[:, 0, 0].min(), contour[:, 0, 0].max()
+            t, b = contour[:, 0, 1].min(), contour[:, 0, 1].max()
+            box = np.array([[l, t], [r, t], [r, b], [l, b]], dtype=np.float32)
+        else:
+            # Make clock-wise order
+            box = np.array(np.roll(box, 4 - box.sum(axis=1).argmin(), 0))
+        boxes.append(2 * box)
+    box_groups.append(np.array(boxes))
+    return box_groups,score_map,link_map
 def build_vgg_backbone(inputs):
     x = make_vgg_block(inputs, filters=64, n=0, pooling=False, prefix='basenet.slice1')
     x = make_vgg_block(x, filters=64, n=3, pooling=True, prefix='basenet.slice1')
@@ -656,7 +736,21 @@ class Detector:
                 yield X, y, sample_weights
             else:
                 yield X, y
-
+    def get_prediction(self,images: typing.List[typing.Union[np.ndarray, str]]):
+         images = [compute_input(tools.read(image)) for image in images]
+         return self.model.predict(np.array(images)
+    def detect_multi_scale(y_preds,detection_threshold=0.7,
+               text_threshold=0.4,
+               link_threshold=0.4,
+               size_threshold=10,
+               **kwargs):
+          boxes,score_map,link_map = getBoxes_multi_scale(y_preds, **kwargs),
+                         detection_threshold=detection_threshold,
+                         text_threshold=text_threshold,
+                         link_threshold=link_threshold,
+                         size_threshold=size_threshold)
+        return boxes,score_map,link_map
+                                   
     def detect(self,
                images: typing.List[typing.Union[np.ndarray, str]],
                detection_threshold=0.7,
