f0861894ceec078ddf7e24cd0c8d46e8677896e8
Ahmed Nady <ahmednady@fci.helwan.edu.eg>
2021-02-03 15:05:20 +0200

    Add Focal Loss

--- keras_ocr/detection.py
+++ keras_ocr/detection.py
@@ -711,13 +711,12 @@ class Detector:
         #self.model.compile(loss='mse', optimizer=opt) #,metrics=['accuracy']
         self.model.compile(loss=self.focal_loss , optimizer=opt) 
     
-     def focal_loss_with_logits(self, logits, targets, alpha, gamma, y_pred):
+    def focal_loss_with_logits(self, logits, targets, alpha, gamma, y_pred):
         weight_a = alpha * (1 - y_pred) ** gamma * targets
         weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
 
         return (tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(
             -logits)) * (weight_a + weight_b) + logits * weight_b
-
     def focal_loss(self, y_true, y_pred):
         y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(),
                                   1 - tf.keras.backend.epsilon())
